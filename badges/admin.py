e0af6ffea361dc2edff9a4264f7c09ed0fa0b02a
JohnGiag <JohnGiag@users.noreply.github.com>
2018-01-14 17:15:03 +0200

    Update admin.py

--- badges/admin.py
+++ badges/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from badges.models import Badge
+from badges.models import Badge,BadgeToUser
 
 class BadgeAdmin(admin.ModelAdmin):
-    fields = ('icon',)
     list_display = ('id','level')
 
+
+class BadgeToUserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'badge','created')
+
+
 admin.site.register(Badge, BadgeAdmin)
+########################
+admin.site.register(BadgeToUser,BadgeToUserAdmin)
